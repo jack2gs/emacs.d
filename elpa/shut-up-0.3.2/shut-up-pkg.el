2ab586eea62b1fb2da06abbef8f2b9f6d05fbd3b
jack2gs <sailor.gao@163.com>
2017-07-16 23:54:51 +0800

    unversion elpa

--- elpa/shut-up-0.3.2/shut-up-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "shut-up" "0.3.2" "Shut up would you!" '((cl-lib "0.3") (emacs "24")) :commit "dccd8f7d6af2dde96718f557b37bc25adc61dd12" :url "http://github.com/rejeep/shut-up.el")
