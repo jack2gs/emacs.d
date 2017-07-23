d02a69c271e1e4bd5cf5872f0c7178f7807f7c74
jack2gs <sailor.g@163.com>
2017-07-23 20:39:43 +0800

    solve that the theme does not works on termil

--- init.el
+++ init.el
@@ -16,12 +16,13 @@
  '(markdown-command "/usr/local/bin/pandoc")
  '(package-selected-packages
    (quote
-    (chinese-fonts-setup cl-lib-highlight org-plus-contrib yasnippet htmlize color-theme-sanityinc-solarized exec-path-from-shell find-file-in-project ## evil markdown-mode markdown-mode+ omnisharp tide))))
+    (pandoc-mode chinese-fonts-setup cl-lib-highlight org-plus-contrib yasnippet htmlize color-theme-sanityinc-solarized exec-path-from-shell find-file-in-project ## evil markdown-mode markdown-mode+ omnisharp tide))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ (if (not window-system) '(default ((t (:background "nil"))))) ;; resolve incorrect Colours in Terminal
  )
 
 (defun setup-tide-mode ()
@@ -129,7 +130,7 @@
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
-;; resolve incorrect Colours in Terminal
+
 
 
 ;; set the theme to solarized-dark
@@ -146,10 +147,6 @@
 ;; set omnisharp-server-executable-path
 (setq omnisharp-server-executable-path "/usr/local/bin/omnisharp")
 
-;; set latex compiler
-;;(require 'ox-latex)
-;;(setq org-latex-compiler 'xelatex)
-
 ;; disable footer when export org to html
 (setq org-html-validation-link nil)
 
@@ -159,13 +156,130 @@
 ;; -----------------------------------------------------------------------------  
 ;; setting font for mac system  
 ;; -----------------------------------------------------------------------------  
-;; Setting English Font   
-(set-face-attribute  
- 'default nil :font "Monaco 12")  
+;; Setting English Font
+(defun s-font()
+  (interactive)
+  ;; font config for org table showing.
+  (set-face-attribute  
+   'default nil :font "Monaco 12")  
 ;; Chinese Font 配制中文字体  
-(dolist (charset '(kana han symbol cjk-misc bopomofo))  
-  (set-fontset-font (frame-parameter nil 'font)  
+  (dolist (charset '(kana han symbol cjk-misc bopomofo))  
+    (set-fontset-font (frame-parameter nil 'font)  
                     charset  
-                    (font-spec :family "Microsoft YaHei" :size 14)))  
+                    (font-spec :family "Microsoft YaHei" :size 14))))  
 ;; tune rescale so that Chinese character width = 2 * English character width
-;; (setq face-font-rescale-alist '(("Monaco" . 1.0) ("Microsoft YaHei" . 1.23)))
+;;(setq face-font-rescale-alist '(("Monaco" . 1.0) ("Microsoft YaHei" . 1.23)))
+(add-to-list 'after-make-frame-functions
+             (lambda (new-frame)
+               (select-frame new-frame)
+               (if window-system
+                   (s-font))))
+(if window-system
+    (s-font))
+
+;; set latex
+(require 'ox-latex)
+(setq org-latex-compiler "xelatex")
+
+(add-to-list 'org-latex-classes
+             '("cn-article"
+               "\\documentclass[10pt,a4paper]{article}
+\\usepackage{graphicx}
+\\usepackage{xcolor}
+\\usepackage{xeCJK}
+\\usepackage{lmodern}
+\\usepackage{verbatim}
+\\usepackage{fixltx2e}
+\\usepackage{longtable}
+\\usepackage{float}
+\\usepackage{tikz}
+\\usepackage{wrapfig}
+\\usepackage{soul}
+\\usepackage{textcomp}
+\\usepackage{listings}
+\\usepackage{geometry}
+\\usepackage{algorithm}
+\\usepackage{algorithmic}
+\\usepackage{marvosym}
+\\usepackage{wasysym}
+\\usepackage{latexsym}
+\\usepackage{natbib}
+\\usepackage{fancyhdr}
+\\usepackage[xetex,colorlinks=true,CJKbookmarks=true,
+linkcolor=blue,
+urlcolor=blue,
+menucolor=blue]{hyperref}
+\\usepackage{fontspec,xunicode,xltxtra}
+\\setmainfont[BoldFont=Adobe Heiti Std]{Adobe Song Std}  
+\\setsansfont[BoldFont=Adobe Heiti Std]{AR PL UKai CN}  
+\\setmonofont{Bitstream Vera Sans Mono}  
+\\newcommand\\fontnamemono{AR PL UKai CN}%等宽字体
+\\newfontinstance\\MONO{\\fontnamemono}
+\\newcommand{\\mono}[1]{{\\MONO #1}}
+\\setCJKmainfont[Scale=0.9]{Adobe Heiti Std}%中文字体
+\\setCJKmonofont[Scale=0.9]{Adobe Heiti Std}
+\\hypersetup{unicode=true}
+\\geometry{a4paper, textwidth=6.5in, textheight=10in,
+marginparsep=7pt, marginparwidth=.6in}
+\\definecolor{foreground}{RGB}{220,220,204}%浅灰
+\\definecolor{background}{RGB}{62,62,62}%浅黑
+\\definecolor{preprocess}{RGB}{250,187,249}%浅紫
+\\definecolor{var}{RGB}{239,224,174}%浅肉色
+\\definecolor{string}{RGB}{154,150,230}%浅紫色
+\\definecolor{type}{RGB}{225,225,116}%浅黄
+\\definecolor{function}{RGB}{140,206,211}%浅天蓝
+\\definecolor{keyword}{RGB}{239,224,174}%浅肉色
+\\definecolor{comment}{RGB}{180,98,4}%深褐色
+\\definecolor{doc}{RGB}{175,215,175}%浅铅绿
+\\definecolor{comdil}{RGB}{111,128,111}%深灰
+\\definecolor{constant}{RGB}{220,162,170}%粉红
+\\definecolor{buildin}{RGB}{127,159,127}%深铅绿
+\\punctstyle{kaiming}
+\\title{}
+\\fancyfoot[C]{\\bfseries\\thepage}
+\\chead{\\MakeUppercase\\sectionmark}
+\\pagestyle{fancy}
+\\tolerance=1000
+[NO-DEFAULT-PACKAGES]
+[NO-PACKAGES]"
+("\\section{%s}" . "\\section*{%s}")
+("\\subsection{%s}" . "\\subsection*{%s}")
+("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+("\\paragraph{%s}" . "\\paragraph*{%s}")
+("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+;; 使用Listings宏包格式化源代码(只是把代码框用listing环境框起来，还需要额外的设置)
+(add-to-list 'org-latex-packages-alist '("" "minted"))
+(setq org-latex-listings 'minted)
+(add-to-list 'org-latex-minted-langs '(csharp "csharp"))
+(setq org-latex-minted-options
+     '(("frame" "lines") ("linenos=true")))
+
+;; Options for \lset command（reference to listing Manual)
+;;(setq org-latex-listings-options
+;;      '(
+;;        ("basicstyle" "\\color{foreground}\\small\\mono")           ; 源代码字体样式
+;;        ("keywordstyle" "\\color{function}\\bfseries\\small\\mono") ; 关键词字体样式
+;;        ("identifierstyle" "\\color{doc}\\small\\mono")
+;;        ("commentstyle" "\\color{comment}\\small\\itshape")         ; 批注样式
+;;        ("stringstyle" "\\color{string}\\small")                    ; 字符串样式
+;;        ("showstringspaces" "false")                                ; 字符串空格显示
+;;        ("numbers" "left")                                          ; 行号显示
+;;        ("numberstyle" "\\color{preprocess}")                       ; 行号样式
+;;        ("stepnumber" "1")                                          ; 行号递增
+;;        ("backgroundcolor" "\\color{background}")                   ; 代码框背景色
+;;        ("tabsize" "4")                                             ; TAB等效空格数
+;;        ("captionpos" "t")                                          ; 标题位置 top or buttom(t|b)
+;;        ("breaklines" "true")                                       ; 自动断行
+;;        ("breakatwhitespace" "true")                                ; 只在空格分行
+;;        ("showspaces" "false")                                      ; 显示空格
+;;        ("columns" "flexible")                                      ; 列样式
+;;        ("frame" "single")                                          ; 代码框：阴影盒
+;;        ("frameround" "tttt")                                       ; 代码框： 圆角
+;;        ("framesep" "0pt")
+;;        ("framerule" "8pt")
+;;        ("rulecolor" "\\color{background}")
+;;        ("fillcolor" "\\color{white}")
+;;        ("rulesepcolor" "\\color{comdil}")
+;;        ("framexleftmargin" "10mm")
+;;        ))
