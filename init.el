f496bd86523d42a07fb94c806e1a956601bc5c12
jack2gs <sailor.g@163.com>
2017-08-02 23:13:45 +0800

    add soft wrap

--- init.el
+++ init.el
@@ -13,16 +13,16 @@
  '(custom-safe-themes
    (quote
     ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(fill-column 120)
  '(markdown-command "/usr/local/bin/pandoc")
  '(package-selected-packages
    (quote
-    (pandoc-mode chinese-fonts-setup cl-lib-highlight org-plus-contrib yasnippet htmlize color-theme-sanityinc-solarized exec-path-from-shell find-file-in-project ## evil markdown-mode markdown-mode+ omnisharp tide))))
+    (visual-fill-column pandoc-mode chinese-fonts-setup cl-lib-highlight org-plus-contrib yasnippet htmlize color-theme-sanityinc-solarized exec-path-from-shell find-file-in-project ## evil markdown-mode markdown-mode+ omnisharp tide))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- (if (not window-system) '(default ((t (:background "nil"))))) ;; resolve incorrect Colours in Terminal
  )
 
 (defun setup-tide-mode ()
@@ -259,5 +259,9 @@ marginparsep=7pt, marginparwidth=.6in}
 ;;        ("framesep=2mm")
 	))
 (setq org-latex-pdf-process
-        '("xelatex -8bit -shell-escape -interaction nonstopmode -output-directory %o %f"))
+      '("xelatex -8bit -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
+;; set up visual-fill-column-mode
+(add-hook 'visual-line-mode-hook 'visual-fill-column-mode)
+(global-visual-line-mode)
 
