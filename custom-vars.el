bcaa7b242b9f6751589cb523b8c48064df7afaa9
jack2gs <sailor.g@163.com>
2024-06-19 22:03:23 +0800

    update

--- custom-vars.el
+++ custom-vars.el
@@ -6,8 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes 'nil)
- '(package-selected-packages
-   '()))
+ '(package-selected-packages nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
